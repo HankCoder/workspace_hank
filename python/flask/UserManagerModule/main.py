9c74a336b4b80c1ec11c1ef900b8f33ac5f31319
ErrareHest <hank199025@gmail.com>
2016-11-22 17:09:09 +0800

    update python/flask/UserManagerModule

--- python/flask/UserManagerModule/main.py
+++ python/flask/UserManagerModule/main.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import render_template
 from flask import redirect
 from db import *
+from model import *
 
 from wtforms import Form, TextField, PasswordField, validators
 app=Flask(__name__)
@@ -10,6 +11,10 @@ app=Flask(__name__)
 class LoginForm(Form):
     username = TextField("username", [validators.Required()])
     password = PasswordField("password", [validators.Required()])
+
+class PublistForm(Form):
+    content = TextField("content", [validators.Required()])
+    sender = TextField("sender", [validators.Required()])
 
 @app.route("/user", methods=['GET', 'POST'])
 def login():
@@ -32,5 +37,15 @@ def register():
         addUser(myForm.username.data, myForm.password.data)
         return "Register Successfully"
     return render_template('index.html', form=myForm)
+
+@app.route("/show", methods=['GET', 'POST'])
+def show():
+    myEntryForm = PublistForm(request.form)
+    l = getAllEntry()
+    if request.method == 'POST':
+        e = Entry(myEntryForm.content.data, myEntryForm.sender.data)
+        e.add()
+        return render_template("show.html", entries=l, form=myEntryForm)
+    return render_template("show.html", entries=l, form=myEntryForm)
 if __name__ == "__main__":
     app.run(port=8000)
