016645c1c68fc726b0fa5e74c2a6b64edd18e4a6
ErrareHest <hank199025@gmail.com>
2016-11-22 14:55:22 +0800

    新增用户注册功能

--- python/flask/UserManagerModule/main.py
+++ python/flask/UserManagerModule/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect
+from db import *
 
 from wtforms import Form, TextField, PasswordField, validators
 app=Flask(__name__)
@@ -17,11 +18,19 @@ def login():
         # username = request.form['username']
         # password = request.form['password']
 
-        if myForm.username.data == "jikexueyuan" and myForm.password.data == "123456" and myForm.validate():
+        if isExisted(myForm.username.data, myForm.password.data):
             return redirect("http://www.baidu.com")
         else:
             message = "Login Failed"
             return render_template('index.html', message=message, form=myForm)
+    return render_template('index.html', form=myForm)
+
+@app.route("/register", methods=['GET', "POST"])
+def register():
+    myForm = LoginForm(request.form)
+    if request.method == 'POST':
+        addUser(myForm.username.data, myForm.password.data)
+        return "Register Successfully"
     return render_template('index.html', form=myForm)
 if __name__ == "__main__":
     app.run(port=8000)
